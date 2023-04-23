e1a2d820fb431a90182ea1c52ab0a293d2b8ca74
sinkZq <121246278+sinkZq@users.noreply.github.com>
2023-04-23 21:38:49 +0200

    Update Static-Whitelist.lua

--- Static-Whitelist.lua
+++ Static-Whitelist.lua
@@ -4,3 +4,22 @@ local m = setmetatable({}, {
 		Whitelisted_UserIds = {4266349299}
 	}
 })
+
+local function IsWhitelisted(Argument : boolean): ()
+	assert(type(Argument) == "boolean", "Boolean type required")
+	if Argument == true 
+		return true
+	elseif Argument == false
+		return false
+	end
+end
+
+if table.find(m.Whitelisted_Hwids, game:GetService("RbxAnalyticsService"):GetClientId()) then
+	return IsWhitelisted(true)
+end
+
+if IsWhitelisted() then
+	print("Whitelisted")
+else
+	print("Not Whitelisted")
+end
