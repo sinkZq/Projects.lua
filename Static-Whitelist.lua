ec65ec41a27b047d9acd4d5cc06634755956f8de
sinkZq <121246278+sinkZq@users.noreply.github.com>
2023-04-23 21:53:30 +0200

    Update Static-Whitelist.lua

--- Static-Whitelist.lua
+++ Static-Whitelist.lua
@@ -1,11 +1,17 @@
-local m = setmetatable({}, {
+local HwidTable = setmetatable({}, {
 	__index = {
 		Whitelisted_Hwids = {"4FE74802-5A7E-4A3E-A223-A4875FD5F1BD"},
-		Whitelisted_UserIds = {4266349299},
 	}
 })
 
-if table.find(m.Whitelisted_Hwids, game:GetService("RbxAnalyticsService"):GetClientId()) then
+local KeyTable = setmetatable({}, {
+	__index = {
+		Keys = {HwidTable.Whitelisted_Hwids[1] = "wyxzeeysyuawzW"},
+	}
+})
+
+
+if table.find(HwidTable.Whitelisted_Hwids, game:GetService("RbxAnalyticsService"):GetClientId()) then
 	print("Whitelisted")
 else
 	print("Not Whitelisted")
