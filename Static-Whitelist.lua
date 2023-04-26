928693fc831f80df04ea6e2d4826b439d5675b17
sinkZq <121246278+sinkZq@users.noreply.github.com>
2023-04-26 15:51:51 +0200

    Update Static-Whitelist.lua

--- Static-Whitelist.lua
+++ Static-Whitelist.lua
@@ -1,36 +1,36 @@
 -- // giving tables metatables bypasses majority of hookmetamethods // __index, __newindex, __namecall
 
 local HwidTable = setmetatable({}, {
-__index = {
-Whitelisted = {"21EE95CC-BE44-4067-94F0-A18216EF2339", 4266349299},
-}
+  __index = {
+  Whitelisted = {"21EE95CC-BE44-4067-94F0-A18216EF2339", 4266349299},
+  }
 })
 
 local KeyTable = setmetatable({}, {
-__index = {
-Key = {"wyxzeeysyuawzW"},
-}
+  __index = {
+  Key = {"wyxzeeysyuawzW"},
+  }
 })
 
 
 -- // anti hooks // http spies
 
 task.spawn(function()
-while task.wait(1) do
-local suc1, err1 = pcall(http.request)
-local suc2, err2 = pcall(game.HttpGet)
+  while task.wait(1) do
+    local suc1, err1 = pcall(http.request)
+    local suc2, err2 = pcall(game.HttpGet)
 
-if (suc1 or suc2) or (err1 ~= "bad argument #1 to 'request' (expected table, got nil)" or err2 ~= "bad argument #1 to 'httpget' (expected userdata, got nil)") then
-game:GetService("Players").LocalPlayer:Kick("HTTP SPY")
-end
-end
+    if (suc1 or suc2) or (err1 ~= "bad argument #1 to 'request' (expected table, got nil)" or err2 ~= "bad argument #1 to 'httpget' (expected userdata, got nil)") then
+      game:GetService("Players").LocalPlayer:Kick("HTTP SPY")
+    end
+  end
 end)
 
 
 -- // checking user whitelist // hwid, userid
 
 if getgenv().Key ~= KeyTable.Key[1] or HwidTable.Whitelisted[1] ~= game:GetService("RbxAnalyticsService"):GetClientId() or HwidTable.Whitelisted[2] ~= game:GetService("Players").LocalPlayer.UserId then
-print("Not Whitelisted")
+  print("Not Whitelisted")
 else
-print("Whitelisted")
+  print("Whitelisted")
 end
