33fe5939b528803e6929bd55dc28f7dec112abb0
sinkZq <121246278+sinkZq@users.noreply.github.com>
2023-04-29 15:44:21 +0200

    Update GetRequest.lua

--- GetRequest.lua
+++ GetRequest.lua
@@ -1,3 +1,3 @@
 -- // test function convertion
 
-print(checkhttp)
+print(hqzwexz,igwiyre,Import,iopvsrt,lmovvbq,aoowteiai,nanweos,hlrdwot,ygqlltg,sviaokq,rsmbizt,swyvgxe,ookwiiapp,butvzeg,bnuscxg,dvluaqz,mlzfcjh,byjaiwn,wjiomax,vhhdehs,ewocpgu,igwiyre)
