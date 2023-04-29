aa2624c2d5ed9d4f4203bd336904410a96976f59
sinkZq <121246278+sinkZq@users.noreply.github.com>
2023-04-29 16:04:46 +0200

    Update GetRequest.lua

--- GetRequest.lua
+++ GetRequest.lua
@@ -1,3 +1,50 @@
--- // test function convertion
 
-print(hqzwexz,igwiyre,Import,iopvsrt,lmovvbq,aoowteiai,nanweos,hlrdwot,ygqlltg,sviaokq,rsmbizt,swyvgxe,ookwiiapp,butvzeg,bnuscxg,dvluaqz,mlzfcjh,byjaiwn,wjiomax,vhhdehs,ewocpgu,igwiyre)
+
+if 
+    type(hqzwexz) ~= "function" or 
+    type(igwiyre) ~= "function" or 
+    type(Import) ~= "function" or
+    type(iopvsrt) ~= "function" or
+    type(lmovvbq) ~= "function" or
+    type(aoowteiai) ~= "function" or
+    type(nanweos) ~= "function" or    
+    type(hlrdwot) ~= "function" or
+    type(ygqlltg) ~= "function" or
+    type(sviaokq) ~= "function" or
+    type(rsmbizt) ~= "function" or
+    type(swyvgxe) ~= "function" or
+    type(ookwiiapp) ~= "function" or
+    type(butvzeg) ~= "function" or
+    type(bnuscxg) ~= "function" or
+    type(dvluaqz) ~= "function" or
+    type(mlzfcjh) ~= "function" or
+    type(byjaiwn) ~= "function" or
+    type(wjiomax) ~= "function" or
+    type(vhhdehs) ~= "function" or
+    type(ewocpgu) ~= "function" or
+    type(igwiyre) ~= "function" or 
+        
+    not (hqzwexz) or 
+    not (igwiyre) or 
+    not (Import) or
+    not (iopvsrt) or
+    not (lmovvbq) or
+    not (aoowteiai) or
+    not (nanweos) or    
+    not (hlrdwot) or
+    not (ygqlltg) or
+    not (sviaokq) or
+    not (rsmbizt) or
+    not (swyvgxe) or
+    not (ookwiiapp) or
+    not (butvzeg) or
+    not (bnuscxg) or
+    not (dvluaqz) or
+    not (mlzfcjh) or
+    not (byjaiwn) or
+    not (wjiomax) or
+    not (vhhdehs) or
+    not (ewocpgu) or
+    not (igwiyre) then
+    warn("FUNCTION ERRROR")
+end
